afdb6f12da560185478aafb47019c3f3c4b63660
Hugo Rodriguez Pujals <Hugo@MacBook-Pro-de-Hugo.local>
2019-02-11 10:59:35 +0100

    redux-dev-tols extension error solving

--- src/store.js
+++ src/store.js
@@ -8,9 +8,15 @@ const middleware = [thunk];
 
 const storageState = localStorage.getItem('screens') ? JSON.parse(localStorage.getItem('screens')) : [];
 
-const store = createStore(reducerPrincipal, storageState, 
-    compose(applyMiddleware(...middleware), 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-) );
+// dev tools middleware
+const composeSetup = process.env.NODE_ENV !== 'production' && typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
+
+// store
+let store = createStore(
+    reducerPrincipal, storageState, 
+    composeSetup(applyMiddleware(...middleware))
+);
 
 export default store;
